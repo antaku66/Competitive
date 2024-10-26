e54f49c3bbad4b9214db896ca49242bc1c1b9bc4
antaku <starry.sky1606@gmail.com>
2024-10-26 22:45:42 +0900

    マクロの整理及び追加

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -2,25 +2,35 @@
 
 using namespace std;
 using ll = long long;
-using pll = pair<ll, ll>;
 using pint = pair<int, int>;
-constexpr double PI = 3.141592653589793;
-constexpr ll MOD = 1e9 + 7;
-constexpr ll mod = 998244353;
-constexpr ll INF = 1LL << 60;
-const vector<int> dx = {1, 0, -1, 0};
-const vector<int> dy = {0, 1, 0, -1};
-#define vec2d(T, name, n, m, ini) vector<vector<T>> name(n, vector<T>(m, ini))
-#define vec3d(T, name, n, m, l, ini) \
-  vector<vector<vector<T>>> name(n, vector<vector<T>>(m, vector<T>(l, ini)))
+using pll = pair<ll, ll>;
+const vector<int> dy = { 0, 1, 0, -1 };
+const vector<int> dx = { 1, 0, -1, 0 };
+
+#define PI  double(3.141592653589793)
+#define MOD int(1000000007)
+#define mod int(998244353)
+#define INF 1LL << 60
+#define VEC2D(T, name, n, m, ini) vector<vector<T>> name(n, vector<T>(m, ini))
+#define VEC3D(T, name, n, m, l, ini) vector<vector<vector<T>>> name(n, vector<vector<T>>(m, vector<T>(l, ini)))
 #define REP(i, e) rep(i, 0, e)
-#define rep(i, s, e) for (ll i = s; i < (e); ++i)
-#define rrep(i, s, e) for (ll i = s; i >= (e); --i)
-#define all(x) (x).begin(), (x).end()
+#define rep(i, s, e) for (int i = s; i < static_cast<int>(e); ++i)
+#define RREP(i, s) rrep(i, s, 0) 
+#define rrep(i, s, e) for (int i = s - 1; i >= static_cast<int>(e); --i)
+#define ITREP(arr) for (auto it = arr.begin(); it != arr.end(); ++it)
+#define ALL(x) (x).begin(), (x).end()
+
+template<typename... Args>
+void Input(Args&... args) { (cin >> ... >> args); }
+template<typename T, typename... Vecs>
+void InputVecs(int n, Vecs&... vecs) { (vecs.resize(n), ...); REP(i, n) { (cin >> ... >> vecs[i]); } }
+#define DIN(type, ...) type __VA_ARGS__; Input(__VA_ARGS__);
+#define VECDIN(type, n, ...) vector<type> __VA_ARGS__; InputVecs<type>(n, __VA_ARGS__);
+#define IOS_ACCELERATION ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+  IOS_ACCELERATION;
 
   return 0;
 }
